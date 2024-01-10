d5278ea8f7b76ccfe07c43798b929eff30e56191
miinuit7 <silk_728@yahoo.co.jp>
2024-01-10 22:53:00 +0900

    コンフリクト修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
