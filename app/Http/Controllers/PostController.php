b4555403b2140214730197a582f2ce30384c9bcd
miinuit7 <silk_728@yahoo.co.jp>
2024-01-12 22:42:12 +0900

    カリキュラム8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,10 @@ class PostController extends Controller
 public function index(Post $post)
  {
     return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
-    }
-    //
+ }
+    
+    public function show(Post $post)
+    {
+      return view('posts.show')->with( ['post' => $post]);
+    }    
 }
